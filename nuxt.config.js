39d741a3ab43eb8cb466c7e77e2f346df662dbfd
James Lovallo <patrickonparker@gmail.com>
2021-10-06 14:14:22 -0400

    set up recipes page

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,31 +1,27 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - macc-cookbook',
-    title: 'macc-cookbook',
+    titleTemplate: "%s - macc-cookbook",
+    title: "macc-cookbook",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [{ src: "~/plugins/clientonly.js", mode: "client" }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -33,21 +29,21 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
+    "@nuxtjs/pwa",
     // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    "@nuxt/content"
   ],
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: "en"
     }
   },
 
@@ -56,9 +52,9 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
+      dark: "auto",
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -74,6 +70,5 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {}
+};
---
+++ pages/recipes/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<h1 style="margin-block: 1em">Recipes</h1>
+		<masonry-layout gap="16px">
+			<v-card v-for="recipe in recipes" :key="recipe.slug" :to="recipe.path">
+				<v-img :src="recipe.image" aspect-ratio="1.333" />
+				<v-card-title>
+					<h3 class="title">{{ recipe.title }}</h3>
+				</v-card-title>
+				<v-card-text>
+					<p class="mb-0">{{ recipe.description }}</p>
+				</v-card-text>
+			</v-card>
+		</masonry-layout>
+	</div>
+</template>
+
+<script>
+export default {
+	async asyncData({ $content, params, error }) {
+		let recipes;
+		try {
+			recipes = await $content("recipes").sortBy("createdAt", "desc").fetch();
+		} catch (e) {
+			error({ message: "Blog Post not found" });
+		}
+
+		return {
+			recipes,
+		};
+	},
+};
+</script>
+
+<style>
+	.title {
+		word-break: keep-all;
+	}
+	.v-card__text {
+		width: auto;
+	}
+	masonry-layout .v-card {
+		margin-bottom: 16px;
+	}
+</style>
