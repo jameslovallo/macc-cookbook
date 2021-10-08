55c1b89637da6b12f50b25f89197fd2dc0d8849c
James Lovallo <patrickonparker@gmail.com>
2021-10-07 22:41:03 -0400

    clean up admin interface

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@
 			</v-btn>
 		</v-app-bar>
 		<v-main>
-			<v-container>
+			<v-container class="mb-16">
 				<Nuxt />
 				<v-btn
 					v-if="$vuetify.breakpoint.smAndDown"
@@ -32,8 +32,36 @@
 				>
 					<v-icon dark>mdi-plus</v-icon>
 				</v-btn>
-				<v-dialog v-model="dialog" width="95vw" max-width="1024px">
-					<Admin />
+				<v-dialog
+					v-model="showAdmin"
+					fullscreen
+					hide-overlay
+					transition="dialog-bottom-transition"
+				>
+					<v-card height="80vh" style="display: flex; flex-direction: column">
+						<v-toolbar
+							flat
+							style="border-bottom: 1px solid rgb(223, 223, 227); flex-grow: 0"
+						>
+							<v-toolbar-title>Add a Recipe</v-toolbar-title>
+							<v-spacer></v-spacer>
+							<v-btn
+								v-if="$vuetify.breakpoint.mdAndUp"
+								@click="showAdmin = false"
+								color="red darken-2"
+								text
+							>
+								<v-icon dark>mdi-close</v-icon>
+								Close
+							</v-btn>
+						</v-toolbar>
+						<iframe
+							:src="adminURL"
+							width="100%"
+							frameborder="0"
+							style="flex-grow: 1"
+						/>
+					</v-card>
 				</v-dialog>
 			</v-container>
 		</v-main>
@@ -43,18 +71,20 @@
 <script>
 export default {
 	data: () => ({
-		dialog: false,
+		adminURL: "",
+		showAdmin: false,
 		signedIn: false,
 		title: "MACC Cookbook",
 	}),
 	methods: {
 		signin() {
 			this.signedIn === true || window.location.hostname === "localhost"
-				? (this.dialog = true)
+				? (this.showAdmin = true)
 				: netlifyIdentity.open();
 		},
 	},
 	mounted() {
+		this.adminURL = window.location.origin + "/admin";
 		let signedIn = localStorage.getItem("gotrue.user");
 		this.signedIn = signedIn ? true : false;
 		netlifyIdentity.on("login", () => {
